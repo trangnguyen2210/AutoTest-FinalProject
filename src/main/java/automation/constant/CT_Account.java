4d2784eb509097c5254d61f7f3656499798c7f54
trang.nguyen <trang.nguyen@xomad.com>
2023-11-24 19:13:35 +0700

    first commit init project

---
+++ src/main/java/automation/constant/CT_Account.java
@@ -0,0 +1,10 @@
+package automation.constant;
+
+import org.openqa.selenium.By;
+
+public class CT_Account {
+public static String webURL = "https://rise.fairsketch.com/signin";
+public static By TEXTBOX_EMAIL = By.id("email");
+public static By TEXTBOX_PASS = By.id("password");
+public static By BTN_LOGIN = By.xpath("//button[@type='submit']");
+}
